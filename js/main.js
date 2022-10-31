1231289468757a0045496ef5cdf67a4982c7658a
Gabriele Cuccarini <gabriele.cuccarini@gmail.com>
2022-10-31 19:08:49 +0100

    Somebody save me

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
+const nomeCognome = prompt("Inserisci Nome e Cognome")
 const kmDaPercorrere = prompt("Inserisci i chilometri da percorrere")
 const age = prompt("Inserisci la tua età")
 
 if ( isNaN( kmDaPercorrere )) {
     alert( "Il numero inserito non è valido" );
 }
-
-
 if ( isNaN( age ))  {
     alert( "Il numero inserito non è valido" );
-  }
+}
+
+let nomeCognomeInput = document.getElementById("nomeCompleto")
+nomeCognomeInput.inntertext += "nomeCognome"
 
 console.log(kmDaPercorrere, age)
 
@@ -29,5 +31,10 @@ if ( age < 18 ) {
     alert("Il tuo biglietto costa " + (scontoOverDeci) + "€. "
     + ("Ti è stato applicato il 40% di sconto"))
 } else {
-alert("Il tuo biglietto costa " + (prezzoDelBiglietto) + "€. " + "Non hai diritto ad alcuno sconto")
+    alert("Il tuo biglietto costa " + (prezzoDelBiglietto) + "€. " + "Non hai diritto ad alcuno sconto")
 }
+
+const generaBiglietto = document.getElementById("generate");
+element.addEventListener("click", function() {
+  alert()
+});
