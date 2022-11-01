53d4dcdaf97695bbb820750465a734c8c87665ac
Gabriele Cuccarini <gabriele.cuccarini@gmail.com>
2022-11-01 11:37:10 +0100

    Moving on

--- js/main.js
+++ js/main.js
@@ -20,21 +20,24 @@ console.log(prezzoDelBiglietto)
 
 if ( age < 18 ) {
     let scontoMinorenni = prezzoDelBiglietto * 0.80 ;
-    const scontoMinorDeci = scontoMinorenni.toFixed(2)
-    console.log(scontoMinorDeci)
-    alert("Il tuo biglietto costa " + (scontoMinorDeci) + "€. "
-    + ("Ti è stato applicato il 20% di sconto"))
+    const prezzoScontato = scontoMinorenni.toFixed(2)
+    console.log(prezzoScontato)
+    // alert("Il tuo biglietto costa " + (prezzoScontato) + "€. "
+    // + ("Ti è stato applicato il 20% di sconto"))
 } else if ( age > 65 ) {
     let scontoOver = prezzoDelBiglietto * 0.60 ;
-    const scontoOverDeci = scontoOver.toFixed(2);
-    console.log(scontoOverDeci)
-    alert("Il tuo biglietto costa " + (scontoOverDeci) + "€. "
-    + ("Ti è stato applicato il 40% di sconto"))
+    const prezzoScontato = scontoOver.toFixed(2);
+    console.log(prezzoScontato)
+    // alert("Il tuo biglietto costa " + (prezzoScontato) + "€. "
+    // + ("Ti è stato applicato il 40% di sconto"))
 } else {
-    alert("Il tuo biglietto costa " + (prezzoDelBiglietto) + "€. " + "Non hai diritto ad alcuno sconto")
+    // alert("Il tuo biglietto costa " + (prezzoDelBiglietto) + "€. " + "Non hai diritto ad alcuno sconto")
 }
 
-const generaBiglietto = document.getElementById("generate");
-element.addEventListener("click", function() {
-  alert()
-});
+const nameAndLast = document.getElementById("nomeCompleto")
+console.log = nameAndLast
+
+// const generaBiglietto = document.getElementById("generate");
+// element.addEventListener("click", function() {
+//   alert()
+// });
